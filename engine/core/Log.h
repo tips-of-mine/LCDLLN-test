79a8da3cc515c9228007681925c3e0dd0295b36a
tips-of-mine <54597409+tips-of-mine@users.noreply.github.com>
2026-02-22 10:46:22 +0100

    Implement core Log interface and macros

--- engine/core/Log.h
+++ engine/core/Log.h
@@ -1,74 +1,48 @@
 #pragma once
 
-#include <cstdlib>
-#include <fstream>
-#include <mutex>
-#include <sstream>
+#include <cstdarg>
+#include <cstdint>
 #include <string>
-#include <utility>
 
 namespace engine::core {
 
-enum class LogLevel {
+enum class LogLevel : uint8_t {
     Trace = 0,
     Debug,
     Info,
     Warn,
     Error,
-    Fatal
+    Fatal,
+    Off
 };
 
-struct LogSettings {
-    LogLevel minLevel = LogLevel::Info;
-    std::string filePath = "engine.log";
-    bool flushAlways = false;
+struct LogConfig {
+    std::string filePath;
+    LogLevel level = LogLevel::Info;
+    bool enableConsole = true;
+    bool enableFile = true;
 };
 
 class Log {
 public:
-    static void Init(const LogSettings& settings = {});
+    static void Init(const LogConfig& config);
     static void Shutdown();
+    static void SetLevel(LogLevel level);
+    static LogLevel GetLevel();
 
-    template <typename... Args>
-    static void Write(LogLevel level, const char* subsystem, Args&&... args) {
-        WriteImpl(level, subsystem, BuildMessage(std::forward<Args>(args)...));
-    }
-
-private:
-    static std::string BuildMessage();
-
-    template <typename T, typename... Rest>
-    static std::string BuildMessage(T&& value, Rest&&... rest) {
-        std::ostringstream oss;
-        Append(oss, std::forward<T>(value));
-        (Append(oss, std::forward<Rest>(rest)), ...);
-        return oss.str();
-    }
-
-    template <typename T>
-    static void Append(std::ostringstream& oss, T&& value) {
-        oss << std::forward<T>(value);
-    }
-
-    static void WriteImpl(LogLevel level, const char* subsystem, const std::string& message);
-    static const char* ToString(LogLevel level);
-
-    static std::mutex s_mutex;
-    static std::ofstream s_file;
-    static LogLevel s_minLevel;
-    static bool s_flushAlways;
-    static bool s_initialized;
+    static void Write(LogLevel level, const char* subsystem, const char* format, ...);
+    static void WriteV(LogLevel level, const char* subsystem, const char* format, va_list args);
 };
 
 } // namespace engine::core
 
-#define LOG_TRACE(subsystem, ...) ::engine::core::Log::Write(::engine::core::LogLevel::Trace, #subsystem, __VA_ARGS__)
-#define LOG_DEBUG(subsystem, ...) ::engine::core::Log::Write(::engine::core::LogLevel::Debug, #subsystem, __VA_ARGS__)
-#define LOG_INFO(subsystem, ...)  ::engine::core::Log::Write(::engine::core::LogLevel::Info, #subsystem, __VA_ARGS__)
-#define LOG_WARN(subsystem, ...)  ::engine::core::Log::Write(::engine::core::LogLevel::Warn, #subsystem, __VA_ARGS__)
-#define LOG_ERROR(subsystem, ...) ::engine::core::Log::Write(::engine::core::LogLevel::Error, #subsystem, __VA_ARGS__)
-#define LOG_FATAL(subsystem, ...) \
+#define LOG_TRACE(Subsystem, ...) ::engine::core::Log::Write(::engine::core::LogLevel::Trace, #Subsystem, __VA_ARGS__)
+#define LOG_DEBUG(Subsystem, ...) ::engine::core::Log::Write(::engine::core::LogLevel::Debug, #Subsystem, __VA_ARGS__)
+#define LOG_INFO(Subsystem, ...) ::engine::core::Log::Write(::engine::core::LogLevel::Info, #Subsystem, __VA_ARGS__)
+#define LOG_WARN(Subsystem, ...) ::engine::core::Log::Write(::engine::core::LogLevel::Warn, #Subsystem, __VA_ARGS__)
+#define LOG_ERROR(Subsystem, ...) ::engine::core::Log::Write(::engine::core::LogLevel::Error, #Subsystem, __VA_ARGS__)
+#define LOG_FATAL(Subsystem, ...) \
     do { \
-        ::engine::core::Log::Write(::engine::core::LogLevel::Fatal, #subsystem, __VA_ARGS__); \
-        std::abort(); \
-    } while (false)
+        ::engine::core::Log::Write(::engine::core::LogLevel::Fatal, #Subsystem, __VA_ARGS__); \
+        ::std::abort(); \
+    } while (0)

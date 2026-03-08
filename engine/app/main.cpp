45dadbbf54adc189ff062e6c3c05d07d1ffcd8b0
hcornet <hcornet@tips-of-mine.fr>
2026-03-08 20:53:18 +0100

    faux depart

--- engine/app/main.cpp
+++ engine/app/main.cpp
@@ -1,4 +1,14 @@
 #include "engine/Engine.h"
+#include "engine/platform/Input.h"
+#include "engine/render/vk/VkSwapchain.h"
+#include "engine/render/ShaderCache.h"
+#include "engine/world/WorldModel.h"
+#include "engine/world/StreamCache.h"
+#include "engine/render/GpuUploadQueue.h"
+#include "engine/world/ChunkBudgetStats.h"
+#include "engine/world/LodConfig.h"
+#include "engine/world/HlodRuntime.h"
+
 #include <cstdio>
 #include <memory>
 
@@ -10,45 +20,45 @@ static int g_result = 1;
 
 static void CreateAndRun(int argc, char** argv)
 {
-    std::fprintf(stderr, "[T] Input\n");         std::fflush(stderr);
+    std::fprintf(stderr, "[T] Input\n");              std::fflush(stderr);
     { engine::platform::Input v; }
-    std::fprintf(stderr, "[T] Input OK\n");      std::fflush(stderr);
+    std::fprintf(stderr, "[T] Input OK\n");           std::fflush(stderr);
 
-    std::fprintf(stderr, "[T] VkSwapchain\n");   std::fflush(stderr);
+    std::fprintf(stderr, "[T] VkSwapchain\n");        std::fflush(stderr);
     { engine::render::VkSwapchain v; }
-    std::fprintf(stderr, "[T] VkSwapchain OK\n");std::fflush(stderr);
+    std::fprintf(stderr, "[T] VkSwapchain OK\n");     std::fflush(stderr);
 
-    std::fprintf(stderr, "[T] ShaderCache\n");   std::fflush(stderr);
+    std::fprintf(stderr, "[T] ShaderCache\n");        std::fflush(stderr);
     { engine::render::ShaderCache v; }
-    std::fprintf(stderr, "[T] ShaderCache OK\n");std::fflush(stderr);
+    std::fprintf(stderr, "[T] ShaderCache OK\n");     std::fflush(stderr);
 
-    std::fprintf(stderr, "[T] World\n");         std::fflush(stderr);
+    std::fprintf(stderr, "[T] World\n");              std::fflush(stderr);
     { engine::world::World v; }
-    std::fprintf(stderr, "[T] World OK\n");      std::fflush(stderr);
+    std::fprintf(stderr, "[T] World OK\n");           std::fflush(stderr);
 
-    std::fprintf(stderr, "[T] StreamCache\n");   std::fflush(stderr);
+    std::fprintf(stderr, "[T] StreamCache\n");        std::fflush(stderr);
     { engine::world::StreamCache v; }
-    std::fprintf(stderr, "[T] StreamCache OK\n");std::fflush(stderr);
+    std::fprintf(stderr, "[T] StreamCache OK\n");     std::fflush(stderr);
 
-    std::fprintf(stderr, "[T] GpuUploadQueue\n");   std::fflush(stderr);
+    std::fprintf(stderr, "[T] GpuUploadQueue\n");     std::fflush(stderr);
     { engine::render::GpuUploadQueue v; }
-    std::fprintf(stderr, "[T] GpuUploadQueue OK\n");std::fflush(stderr);
+    std::fprintf(stderr, "[T] GpuUploadQueue OK\n");  std::fflush(stderr);
 
-    std::fprintf(stderr, "[T] ChunkBudgetStats\n"); std::fflush(stderr);
+    std::fprintf(stderr, "[T] ChunkBudgetStats\n");   std::fflush(stderr);
     { engine::world::ChunkBudgetStats v; }
     std::fprintf(stderr, "[T] ChunkBudgetStats OK\n");std::fflush(stderr);
 
-    std::fprintf(stderr, "[T] LodConfig\n");     std::fflush(stderr);
+    std::fprintf(stderr, "[T] LodConfig\n");          std::fflush(stderr);
     { engine::world::LodConfig v; }
-    std::fprintf(stderr, "[T] LodConfig OK\n");  std::fflush(stderr);
+    std::fprintf(stderr, "[T] LodConfig OK\n");       std::fflush(stderr);
 
-    std::fprintf(stderr, "[T] HlodRuntime\n");   std::fflush(stderr);
+    std::fprintf(stderr, "[T] HlodRuntime\n");        std::fflush(stderr);
     { engine::world::HlodRuntime v; }
-    std::fprintf(stderr, "[T] HlodRuntime OK\n");std::fflush(stderr);
+    std::fprintf(stderr, "[T] HlodRuntime OK\n");     std::fflush(stderr);
 
-    std::fprintf(stderr, "[T] Engine()\n");      std::fflush(stderr);
+    std::fprintf(stderr, "[T] Engine()\n");           std::fflush(stderr);
     g_engine = std::make_unique<engine::Engine>(argc, argv);
-    std::fprintf(stderr, "[T] Engine OK\n");     std::fflush(stderr);
+    std::fprintf(stderr, "[T] Engine OK\n");          std::fflush(stderr);
     g_result = g_engine->Run();
 }
 

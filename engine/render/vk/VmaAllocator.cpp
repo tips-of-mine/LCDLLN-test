5d418d5db8325b3af1e3002b995f4ac7452de666
tips-of-mine <54597409+tips-of-mine@users.noreply.github.com>
2026-03-09 14:42:53 +0100

    Update VmaAllocator.cpp

--- engine/render/vk/VmaAllocator.cpp
+++ engine/render/vk/VmaAllocator.cpp
@@ -1,4 +1,3 @@
 // Single translation unit that defines the Vulkan Memory Allocator implementation.
 // Include path to vk_mem_alloc.h must be provided by CMake (VMA_SOURCE_DIR/include).
-#define VMA_IMPLEMENTATION
 #include "vk_mem_alloc.h"

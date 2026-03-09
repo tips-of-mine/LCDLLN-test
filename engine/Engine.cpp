8b9507c444851996518d27d067214e5158e72cfc
tips-of-mine <54597409+tips-of-mine@users.noreply.github.com>
2026-03-09 13:16:17 +0100

    Update Engine.cpp

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -10,7 +10,7 @@
 #include <vulkan/vulkan.h>
 
 #define VMA_STATIC_VULKAN_FUNCTIONS  0
-#define VMA_DYNAMIC_VULKAN_FUNCTIONS 0
+#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
 #define VMA_IMPLEMENTATION
 #include <vk_mem_alloc.h>
 

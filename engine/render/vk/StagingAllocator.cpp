f60742a3e82c1669018c939186e2370a9f64c530
tips-of-mine <54597409+tips-of-mine@users.noreply.github.com>
2026-03-09 13:29:19 +0100

    Update StagingAllocator.cpp

--- engine/render/vk/StagingAllocator.cpp
+++ engine/render/vk/StagingAllocator.cpp
@@ -1,8 +1,6 @@
 #include "engine/render/vk/StagingAllocator.h"
 #include "engine/core/Log.h"
 
-#define VMA_STATIC_VULKAN_FUNCTIONS  0
-#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
 #include <vk_mem_alloc.h>
 #include <cstddef>
 

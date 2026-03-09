b0ee1e9c3d954bfebf7892dabc5eb5071a5bf876
tips-of-mine <54597409+tips-of-mine@users.noreply.github.com>
2026-03-09 13:31:21 +0100

    Update SpecularPrefilterPass.cpp

--- engine/render/SpecularPrefilterPass.cpp
+++ engine/render/SpecularPrefilterPass.cpp
@@ -3,8 +3,6 @@
 #include "engine/core/Log.h"
 
 #include <vulkan/vulkan.h>
-#define VMA_STATIC_VULKAN_FUNCTIONS  0
-#define VMA_DYNAMIC_VULKAN_FUNCTIONS 0
 #include <vk_mem_alloc.h>
 #include <cstring>
 

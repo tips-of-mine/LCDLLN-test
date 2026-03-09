bfb8c57c548a12be2be230be53c3b0f35699bcc8
tips-of-mine <54597409+tips-of-mine@users.noreply.github.com>
2026-03-09 14:05:19 +0100

    Update Engine.cpp

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -9,9 +9,6 @@
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
 
-#define VMA_STATIC_VULKAN_FUNCTIONS  0
-#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
-#define VMA_IMPLEMENTATION
 #include <vk_mem_alloc.h>
 
 #include <chrono>
